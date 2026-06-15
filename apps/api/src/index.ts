7a9ece9b9bb3c7439fb0260929ef0f773a3da6ff
Victor Santos <santtosvictor42@gmail.com>
2026-06-15 13:37:27 -0300

    feat: encapsular lógica de inicialização do servidor em uma função assíncrona

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,13 @@
 import { env } from "./shared/env/env";
 import { buildServer } from "./app";
 
-const app = await buildServer();
+(async () => {
+  const app = await buildServer();
 
-try {
-  await app.listen({ port: env.PORT, host: "0.0.0.0" });
-} catch (error) {
-  app.log.error(error);
-  process.exit(1);
-}
+  try {
+    await app.listen({ port: env.PORT, host: "0.0.0.0" });
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+})();
