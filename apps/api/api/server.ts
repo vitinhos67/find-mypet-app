0d0faf9e223ede106fd5c89516e965f34c77c343
Victor Santos <santtosvictor42@gmail.com>
2026-06-14 21:38:41 -0300

    feat: remover arquivo handler.ts e sua lógica de manipulação de requisições

--- apps/api/api/server.ts
+++ apps/api/api/server.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { buildServer } from "./src/server";
+import { buildServer } from "../src/server";
 
 type FastifyApp = Awaited<ReturnType<typeof buildServer>>;
 
